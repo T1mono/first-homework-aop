0fc31a7235172394ce08622e95eecac93aac86a9
TKHidirov <timos.gooogle@gmail.com>
2024-04-14 17:00:00 +0500

    git init

---
+++ src/main/java/ru/t1/firsthomeworkaop/FirstHomeworkAopApplication.java
@@ -0,0 +1,13 @@
+package ru.t1.firsthomeworkaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstHomeworkAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstHomeworkAopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/t1/firsthomeworkaop/FirstHomeworkAopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1.firsthomeworkaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstHomeworkAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
